9f69c2d16f8221b83b058093e769d4ae717cf57d
v-rowjee <vedrowjee@gmail.com>
2022-05-07 15:21:46 +0400

    bootstrap

--- includes/footer.php
+++ includes/footer.php
@@ -2,11 +2,13 @@
     <script src="js/jquery-3.6.0.min.js"></script>
 
     <!-- Bootstrap -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    
+    <script src="bootstrap/bootstrap.bundle.min.js"></script>
+
     <!-- Snackbar -->
-    <script src="js/snackbar.min.js"></script>
+    <!-- <script src="js/snackbar.min.js"></script> -->
+    
+    <!-- SimpleBar -->
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/simplebar/5.3.6/simplebar.min.js" integrity="sha512-rn0CaCtyheymRT43p0ZuFH0SvNriLU5cuJpY2f4IcyLrbHzJdsIF1dygi9oNha1BhHagO8U+h2xRWCosYf9gCg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script> -->
 
     <!-- My script -->
     <script src="js/script.js" type="text/javascript"></script>

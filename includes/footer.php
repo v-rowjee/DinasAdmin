194c24837ff30c37016e43628886d6d53155f0c1
v-rowjee <vedrowjee@gmail.com>
2022-05-26 15:55:51 +0400

    dlt stuff

--- includes/footer.php
+++ includes/footer.php
@@ -4,9 +4,6 @@
     <!-- Bootstrap -->
     <script src="bootstrap/bootstrap.bundle.min.js"></script>
 
-    <!-- Snackbar -->
-    <!-- <script src="js/snackbar.min.js"></script> -->
-    
     <!-- My script -->
     <script src="js/script.js" type="text/javascript"></script>
   </body>

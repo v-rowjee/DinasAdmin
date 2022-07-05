9ab1c9c759fb40b894bb7bea406b2cf5d8a64017
v-rowjee <vedrowjee@gmail.com>
2022-07-05 18:07:25 +0400

    menu

--- ajax/search-menu.php
+++ ajax/search-menu.php
@@ -1,16 +1,19 @@
 <?php
-session_start();
+// session_start();
 
-use Opis\JsonSchema\Schema;
-use Opis\JsonSchema\Validator;
-use Opis\JsonSchema\Errors\ErrorFormatter;
+// use Opis\JsonSchema\Schema;
+// use Opis\JsonSchema\Validator;
+// use Opis\JsonSchema\Errors\ErrorFormatter;
 
-require '../vendor/autoload.php';
+// require '../vendor/autoload.php';
 require '../config/db_connect.php';
 
-if(isset($_POST['search']) && isset($_POST['category'])){
+if(isset($_POST['search']) || isset($_POST['category'])){
   $search = $_POST['search'];
   $category = $_POST['category'];
+
+  $return_array = array("msg"=>"","data"=>"");
+
 
   $sql = "SELECT * FROM menu WHERE name LIKE :search AND category LIKE :category ORDER BY category DESC, name ASC";
   $query = $conn->prepare($sql);
@@ -21,48 +24,18 @@ if(isset($_POST['search']) && isset($_POST['category'])){
 
   if($query->rowCount() > 0){
 
-    // validateMenu($query);
-
-    while ($item=$query->fetch(PDO::FETCH_ASSOC)) {
-        echo '
-            <div class="col-sm-6 col-md-4 col-lg-3">
-              <a href="menu.php?id='.$item['id'].'" class="card-link">
-                <div class="card card-shadow">
-                  <img
-                      src="images/menu/'.checkImage($item['img']).'"
-                      class="card-img-top"
-                      alt="'.$item['name'].'"
-                  />
-                  <div class="card-img-overlay">
-                    <p class="item-cat">'.$item['category'].'</p>
-                  </div>
-                  <div class="card-body">
-                      <h5 class="card-title">'.$item['name'].'</h5>
-                      <p class="card-text mb-2">'.$item['caption'].'</p>
-                      <h6 class="price">Rs '.$item['price'].'</h6>
-
-                      <a href="menu.php?id='.$item['id'].'" class="btn btn-primary">
-                        Edit <i class="bx bxs-edit pt-2"></i>
-                      </a>
-
-                  </div>
-                </div>
-              </a>
-            </div>
-        ';
-    }
+    $array_result = $query->fetchAll(PDO::FETCH_ASSOC);
+    $return_array['msg'] = "success";
+    $return_array['data'] = $array_result;
   }
   else{
-    echo '<div class="col align-self-center text-center"><p class="msg">No Search Result for: "'.$search.'"</p></div>';
+    $return_array['msg'] = "error";
+    $return_array['data'] = $search;
   }
   
-}else{
-  // order by descending category (z-a) and descending id (most recent to oldest)
-  $sql = "SELECT * FROM menu ORDER BY category DESC, name ASC"; 
-  $query = $conn->prepare($sql);
-  $query->execute();
+  header('Content-Type: application/json');
+  echo json_encode($return_array);
 }
-
 
 
 function checkImage($img){
@@ -70,31 +43,3 @@ function checkImage($img){
       return $img;
     }else return 'default.jpg';
 }
-
-function validateMenu($query){
-    $arrayResult = $query->fetchAll(PDO::FETCH_ASSOC);
-    $json = json_encode(array('menu'=>$arrayResult), JSON_NUMERIC_CHECK);
-    $data = json_decode($json);
-
-    $validator = new Validator();
-
-    $resolver = $validator->resolver();
-    $resolver->registerPrefix("http://example.com/","../schema");
-
-
-    $result = $validator->validate($data,"http://example.com/menu.json");
-
-    if($result->isValid()){
-      header('Content-Type: application/json');
-      echo $data;
-    }
-    else{
-      print_r((new ErrorFormatter())->format($result->error()));
-      header('HTTP/1.1 500 Internal Server Error \n'.$result->error());
-      // exit;
-    }
-
-
-}
-
-?>
--- menu.php
+++ menu.php
@@ -47,7 +47,7 @@ if(isset($_GET['id'])){
 <?php } ?>
 
 <script>
-  searchMenu();
+  searchMenu()
 
   function searchMenu(){
     $.ajax({
@@ -57,8 +57,53 @@ if(isset($_GET['id'])){
             search: $("#search").val(),
             category: $('#category').val()
         },
-        success:function(data){
-            $("#output").html(data);
+        accepts: "application/json",
+        success: function(data){
+
+            if(data.msg == "success"){   
+              $('#output').html('') // reset output
+
+              $.each(data.data, (i,item)=>{
+                $('#output').append(`
+                  <div class="col-sm-6 col-md-4 col-lg-3">
+                    <a href="menu.php?id=${item.id}" class="card-link">
+                      <div class="card card-shadow">
+                        <img
+                            src="images/menu/${item.img}"
+                            class="card-img-top"
+                            alt="${item.name}"
+                        />
+                        <div class="card-img-overlay">
+                          <p class="item-cat">${item.category}</p>
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">${item.name}</h5>
+                            <p class="card-text mb-2">${item.caption}</p>
+                            <h6 class="price">Rs ${item.price}</h6>
+
+                            <a href="menu.php?id=${item.id}" class="btn btn-primary">
+                              Edit <i class="bx bxs-edit pt-2"></i>
+                            </a>
+
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                `)
+      
+              })
+
+            }
+            else if(data.msg == "error"){
+              $('#output').html(`
+              <div class="col align-self-center text-center">
+                <img src="images/illustration-search.png" class="w-25">
+                </br>
+                <p class="opacity-100">No Search Result</p>
+                </div>
+
+              `)
+            }
         }
     });
   }
